d7133a2117292ea1a311a3c675bc85c7e6dad879
Erny Sans <ernysans@icloud.com>
2024-01-27 07:33:04 -0500

    User Preview Component

---
+++ src/components/shared/UserPreview.js
@@ -0,0 +1,31 @@
+import PropTypes from "prop-types";
+import {connect} from "react-redux";
+import Avatar from "./Avatar";
+
+const UserPreview = ({user, featured = false}) => {
+  return (
+    <div className={
+      featured ? `user-preview user-preview-featured` : "user-preview"
+    }>
+      <Avatar url={user.avatarURL}></Avatar>
+      <span className={
+        featured ? "mdc-typography--headline1" : "mdc-typography--body1"
+      }>{user.name}</span>
+    </div>
+  );
+};
+
+UserPreview.propTypes = {
+  uid: PropTypes.string.isRequired,
+  featured: PropTypes.bool,
+};
+
+const mapStateToProps = ({users}, ownProps) => {
+  const {uid} = ownProps;
+  const user = users[uid];
+  return {
+    user,
+  };
+};
+
+export default connect(mapStateToProps)(UserPreview);
